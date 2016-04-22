c4ca50268af8ceb35819b1bd61964b2c35127687
Chrysthoper <chrys_18@hotmail.com>
2016-04-21 21:49:05 -0700

    First Commit

---
+++ server.js
@@ -0,0 +1,180 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var nodemailer = require('nodemailer');
+
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
+app.use("/font", express.static(__dirname + '/font'));
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+var transport = nodemailer.createTransport("SMTP", {
+    host: "smtp-mail.outlook.com", // hostname
+    secureConnection: false, // TLS requires secureConnection to be false
+    port: 587, // port for secure SMTP
+    auth: {
+        user: "chrys_18@hotmail.com",
+        pass: "ISC0812abc"
+    },
+    tls: {
+        ciphers:'SSLv3'
+    }
+});
+/*
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: '"Chrys Rangel 👥" <chrys_18@hotmail.com>', // sender address
+    to: 'chrysisc18@gmail.com', // list of receivers
+    subject: 'Hello ✔', // Subject line
+    text: 'Hola 🐴', // plaintext body
+    html: '<b>Hello world 🐴</b>' // html body
+};
+*/
+app.get('/send',function(req,res){
+
+	var mailOptions={
+		from: '"Chrys Rangel 👥" <chrys_18@hotmail.com>', // sender address
+	   to : req.query.to,
+	   subject : req.query.subject,
+	   text : req.query.text
+	}
+	console.log(mailOptions);
+	transport.sendMail(mailOptions, function(error, response){
+	if(error){
+	console.log(error);
+	res.end("error");
+	}else{
+	console.log("Message sent: " + response.message);
+	res.end("sent");
+	}
+	});
+
+});
+
+
+/*
+app.get('/js/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/js/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+app.get('/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname,
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+app.get('/css/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/css/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+app.get('/font/roboto/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/font/roboto/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+
+app.get('/font/material-design-icons/:name', function (req, res, next) {
+
+  var options = {
+    root: __dirname + '/font/material-design-icons/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+
+  var fileName = req.params.name;
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else {
+      console.log('Sent:', fileName);
+    }
+  });
+
+});
+*/
+app.listen(8000);
